1f6f99a20872c4450305912268fef6272c2e999a
Ayman <ayman.patel97@outlook.com>
2023-05-02 18:40:06 +0530

    Delete zodFile8.js

--- dist/withZod/zodFile8.js
+++
@@ -1,7 +0,0 @@
-console.log('*** zodString8.js')
-function myStringFunctionCDN(temp) {
-    const stringZodValidator = z.string().refine((val) => val.length <= 255, {
-        message: "String can't be more than 255 characters",
-    })
-    stringZodValidator.parse(temp)
-}
